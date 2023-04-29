f77ae8e7544522a88cbbbcc8a85c21ce3c9011a5
lavanmetta <lavanmetta111@gmail.com>
2023-04-29 23:25:55 +0530

    Quiz

--- Q - Quiz App/src/Components/QuizApp/Quiz.jsx
+++ Q - Quiz App/src/Components/QuizApp/Quiz.jsx
@@ -2,21 +2,34 @@ import React, { useEffect, useState } from "react";
 import "./Quiz.css";
 import QuestionsList from "../Data";
 
-
-
 function Quiz() {
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
 
   useEffect(() => {
-    const listOfData = QuestionsList()
-     setData(listOfData)
-  }, [])
+    const listOfData = QuestionsList();
+    setData(listOfData);
+  }, []);
+
+  const nextButtonHandler = () => {
+    if (currentQuestion < data.length - 1) {
+      setCurrentQuestion(currentQuestion + 1);
+    }
+
+    console.log(currentQuestion);
+  };
 
   return (
     <div>
-      {data.map((each) => (
-        <p>{each.question}</p>
-      ))}
+      {data[currentQuestion] && (
+        <div key={data[currentQuestion].id}>
+          <p>{data[currentQuestion].question}</p>
+          {data[currentQuestion].answers.map((answer) => (
+            <span key={answer}>{answer}</span>
+          ))}
+        </div>
+      )}
+      <button onClick={nextButtonHandler}>Next</button>
     </div>
   );
 }
